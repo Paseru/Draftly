655339451d0cf981e8c787912fb313fa21efa6ac
Paseru <hyrakalache@gmail.com>
2025-12-03 02:14:09 +0100

    Fix build error: add react-syntax-highlighter types and update baseline-browser-mapping

---
+++ src/types/react-syntax-highlighter.d.ts
@@ -0,0 +1,10 @@
+declare module 'react-syntax-highlighter' {
+  export const Prism: any;
+  export const Light: any;
+}
+
+declare module 'react-syntax-highlighter/dist/esm/styles/prism' {
+  const style: any;
+  export default style;
+  export const vscDarkPlus: any;
+}
